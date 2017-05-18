cb26c4a12331ad27e000c0c0c40ce13375879a8e
halucka <halucka@gmail.com>
2017-05-18 16:14:01 +0200

    formatting changes to conform to PEP 8

--- seeTemporarySolution.py
+++ seeTemporarySolution.py
@@ -1,38 +1,37 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+a = np.load("temporarySolution.npy")
+# dx = np.load("temporaryDX.npy")
+# dy = np.load("temporaryDY.npy")
 
-a = np.load("finalSolution.npy")
-#dx = np.load("temporaryDX.npy")
-#dy = np.load("temporaryDY.npy")
 
-def plot(x,title):
+def plot(x, title):
     
     fig, axarr = plt.subplots(2, 2)
-    img1 = axarr[0, 0].imshow(x[:,:,0].T, interpolation='nearest')
+    img1 = axarr[0, 0].imshow(x[:, :, 0].T, interpolation='nearest')
     axarr[0, 0].set_title(title[0])
-    plt.axes(axarr[0,0])
+    plt.axes(axarr[0, 0])
     fig.colorbar(img1)
     
-    img2 = axarr[0, 1].imshow(x[:,:,1].T, interpolation='nearest')
+    img2 = axarr[0, 1].imshow(x[:, :, 1].T, interpolation='nearest')
     axarr[0, 1].set_title(title[1])
-    plt.axes(axarr[0,1])
+    plt.axes(axarr[0, 1])
     fig.colorbar(img2)
     
-    img3 = axarr[1, 0].imshow(x[:,:,2].T, interpolation='nearest')
+    img3 = axarr[1, 0].imshow(x[:, :, 2].T, interpolation='nearest')
     axarr[1, 0].set_title(title[2])
-    plt.axes(axarr[1,0])
+    plt.axes(axarr[1, 0])
     fig.colorbar(img3)
     
-    
-    img4 = axarr[1, 1].imshow(x[:,:,3].T, interpolation='nearest')
+    img4 = axarr[1, 1].imshow(x[:, :, 3].T, interpolation='nearest')
     axarr[1, 1].set_title(title[3])
-    plt.axes(axarr[1,1])
+    plt.axes(axarr[1, 1])
     fig.colorbar(img4)
     # adjust spacing between subplots so that things don't overlap
     fig.tight_layout()
 
-plot(a,["Re(Deltax)","Im(Deltax)","Re(Deltay)","Im(Deltay)"] )
-#plot(dx,["dx Re(Deltax)","dx Im(Deltax)","dx Re(Deltay)","dx Im(Deltay)"] )
-#plot(dy,["dy Re(Deltax)","dy Im(Deltax)","dy Re(Deltay)","dy Im(Deltay)"] )
+plot(a, ["Re(Deltax)", "Im(Deltax)", "Re(Deltay)", "Im(Deltay)"])
+# plot(dx,["dx Re(Deltax)","dx Im(Deltax)","dx Re(Deltay)","dx Im(Deltay)"] )
+# plot(dy,["dy Re(Deltax)","dy Im(Deltax)","dy Re(Deltay)","dy Im(Deltay)"] )
 plt.show()
