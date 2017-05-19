e27be7432f0dc60c52b6819503eab7ae728f47ea
halucka <halucka@gmail.com>
2017-05-19 14:56:08 +0200

    little formatting

--- minimize.py
+++ minimize.py
@@ -77,24 +77,24 @@ if hasTiltedEdge:
         emptyRangeX.append(slice(0, lowX[y]))
 
 
-def plot(x, title):
+def plot(xdata, title):
     fig, axarr = plt.subplots(2, 2)
-    img1 = axarr[0, 0].imshow(x[:, :, 0].T, interpolation='nearest')
+    img1 = axarr[0, 0].imshow(xdata[:, :, 0].T, interpolation='nearest')
     axarr[0, 0].set_title(title[0])
     plt.axes(axarr[0, 0])
     fig.colorbar(img1)
 
-    img2 = axarr[0, 1].imshow(x[:, :, 1].T, interpolation='nearest')
+    img2 = axarr[0, 1].imshow(xdata[:, :, 1].T, interpolation='nearest')
     axarr[0, 1].set_title(title[1])
     plt.axes(axarr[0, 1])
     fig.colorbar(img2)
 
-    img3 = axarr[1, 0].imshow(x[:, :, 2].T, interpolation='nearest')
+    img3 = axarr[1, 0].imshow(xdata[:, :, 2].T, interpolation='nearest')
     axarr[1, 0].set_title(title[2])
     plt.axes(axarr[1, 0])
     fig.colorbar(img3)
 
-    img4 = axarr[1, 1].imshow(x[:, :, 3].T, interpolation='nearest')
+    img4 = axarr[1, 1].imshow(xdata[:, :, 3].T, interpolation='nearest')
     axarr[1, 1].set_title(title[3])
     plt.axes(axarr[1, 1])
     fig.colorbar(img4)
